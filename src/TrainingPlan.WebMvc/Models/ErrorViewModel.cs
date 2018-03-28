12fdcabc7e4d52c5a85e29efb1a8b18fc22c9d9a
Alexandre Lepage <alexandre.lepage@larochellegc.com>
2018-03-28 14:13:50 -0400

    - Initial commit (base solution structure).

---
+++ src/TrainingPlan.WebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrainingPlan.WebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
